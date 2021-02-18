37db5bf4d26abe8a8dff79a814b64be9048cca20
Aman Upadhyay <76415079+B4DSH4H-AMAN@users.noreply.github.com>
2021-02-18 20:10:00 +0530

    Delete School Exam Online directory

--- School Exam Online/database/dbConnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-// dbConnection is Variable which stores Database Connection Details.
-$dbConnection = mysqli_connect('localhost', 'root', '', 'exam_portal');
-// If Database connection Fails it will through an Error with Details and Connection will be closed with Die() Function.
-if($dbConnection === false){
-    die(mysqli_connect_error());
-}
-?>
--- School Exam Online/server/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-unset($_SESSION["ID"]);
-unset($_SESSION["Name"]);
-unset($_SESSION["Role"]);
-header("Location:../index.php");
-?>
